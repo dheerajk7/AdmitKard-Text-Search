26b5cc88659b41f1ebc86a5cb93073fb940c956d
Dheeraj Kushwah <dheeraj77253@gmail.com>
2020-09-11 18:20:39 +0530

    add search and question controller

--- controllers/api/v1/search_controller.js
+++ controllers/api/v1/search_controller.js
@@ -1,9 +1,23 @@
 const Tags = require("../../../models/tags");
 
 module.exports.search = async function (request, response) {
+  let searchTagList = request.body.searchKey.split(" ");
+  let tags = await Tags.find({ tag: { $in: searchTagList } }).populate(
+    "queries"
+  );
+  var querySetId = new Set();
+  let queryList = [];
+  for (let tag of tags) {
+    for (let query of tag.queries) {
+      if (!querySetId.has(query._id)) {
+        queryList.push(query);
+        querySetId.add(query._id);
+      }
+    }
+  }
   return response.status(200).json({
     success: true,
-    message: "Search",
+    message: "Result Received",
     body: {
       question: queryList,
     },
