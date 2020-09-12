5d13c9b2b0b7b24c83fe6af9b2de2404fa8b0348
Dheeraj Kushwah <dheeraj77253@gmail.com>
2020-09-12 12:35:48 +0530

    add regex to search pattern in question if tag not matched

--- controllers/api/v1/question_controller.js
+++ controllers/api/v1/question_controller.js
@@ -2,23 +2,31 @@ const Question = require("../../../models/question");
 const Tags = require("../../../models/tags");
 
 module.exports.create = async function (request, response) {
-  let question = await Question.create({
-    query: request.body.query,
-    topic: request.body.topic,
-  });
-  let tagList = request.body.tags;
-  for (let i = 0; i < tagList.length; i++) {
-    tag = await Tags.findOne({ tag: tagList[i] });
-    if (!tag) {
-      tag = await Tags.create({ tag: tagList[i] });
+  try {
+    console.log(request.body);
+    let question = await Question.create({
+      query: request.body.query,
+      topic: request.body.topic,
+    });
+    let tagList = request.body.tagList;
+    for (let i = 0; i < tagList.length; i++) {
+      tag = await Tags.findOne({ tag: tagList[i] });
+      if (!tag) {
+        tag = await Tags.create({ tag: tagList[i].toLowerCase() });
+      }
+      await tag.queries.push(question.id);
+      await question.tags.push(tag.id);
+      await tag.save();
+      await question.save();
     }
-    await tag.queries.push(question.id);
-    await question.tags.push(tag.id);
-    await tag.save();
-    await question.save();
+    return response.status(200).json({
+      success: true,
+      message: "Query created Successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    return response
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
   }
-  return response.status(200).json({
-    success: true,
-    message: "Query created Successfully",
-  });
 };
--- controllers/api/v1/search_controller.js
+++ controllers/api/v1/search_controller.js
@@ -1,25 +1,52 @@
 const Tags = require("../../../models/tags");
+const Question = require("../../../models/question");
 
 module.exports.search = async function (request, response) {
-  let searchTagList = request.body.searchKey.split(" ");
-  let tags = await Tags.find({ tag: { $in: searchTagList } }).populate(
-    "queries"
-  );
-  var querySetId = new Set();
-  let queryList = [];
-  for (let tag of tags) {
-    for (let query of tag.queries) {
-      if (!querySetId.has(query._id)) {
-        queryList.push(query);
-        querySetId.add(query._id);
+  try {
+    let searchTagList = request.body.searchKey.split(" ");
+    let newSearchTagList = [];
+    for (let i = 0; i < searchTagList.length; i++) {
+      newSearchTagList.push(searchTagList[i].toLowerCase());
+    }
+    let tags = await Tags.find({ tag: { $in: newSearchTagList } }).populate(
+      "queries"
+    );
+    var querySetId = new Set();
+    let queryList = [];
+    for (let tag of tags) {
+      for (let query of tag.queries) {
+        if (!querySetId.has(query._id)) {
+          queryList.push(query);
+          querySetId.add(query._id);
+        }
       }
     }
+    if (queryList.length > 0) {
+      return response.status(200).json({
+        success: true,
+        message: "Result Received",
+        data: {
+          questions: queryList,
+        },
+      });
+    } else {
+      let regEx = new RegExp(request.body.searchKey, "i");
+      let question = await Question.find({
+        query: { $regex: regEx },
+      });
+      return response.status(200).json({
+        success: true,
+        message: "Question Received",
+        data: {
+          questions: question,
+        },
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return response.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
   }
-  return response.status(200).json({
-    success: true,
-    message: "Result Received",
-    body: {
-      question: queryList,
-    },
-  });
 };
